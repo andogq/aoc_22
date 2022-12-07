033298f9986b98aa89b925d1c7eae2d54a48d2c3
Tom Anderson <tom@ando.gq>
2022-12-07 23:12:19 +1100

    Clean up day 07
    Simplify parsing and size calculation of directories

--- src/problems/day_07.rs
+++ src/problems/day_07.rs
@@ -1,19 +1,6 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::collections::HashMap;
 
 use crate::day::Day;
-
-type Directory = HashMap<String, Entry>;
-
-#[derive(Clone)]
-pub struct FlattenedDirectories {
-    pub dirs: Vec<usize>,
-    pub size: usize,
-}
-
-pub enum Entry {
-    File(usize),
-    Dir(Rc<RefCell<Directory>>),
-}
 
 const MAX_DIR_SIZE: usize = 100000;
 const DISK_SIZE: usize = 70000000;
@@ -21,22 +8,19 @@ const UPDATE_SIZE: usize = 30000000;
 
 pub struct Day07;
 impl Day for Day07 {
-    type Input = FlattenedDirectories;
+    type Input = HashMap<String, usize>;
     type Output = usize;
 
     fn part_1(input: Self::Input) -> Self::Output {
         // Find directories under MAX_DIR_SIZE
-        input
-            .dirs
-            .into_iter()
-            .filter(|&dir| dir <= MAX_DIR_SIZE)
-            .sum()
+        input.into_values().filter(|&dir| dir <= MAX_DIR_SIZE).sum()
     }
 
     fn part_2(input: Self::Input) -> Self::Output {
-        let remaining_size = DISK_SIZE - input.size;
+        let root_size = *input.get("/").unwrap();
+        let remaining_size = DISK_SIZE - root_size;
 
-        input.dirs.into_iter().fold(input.size, |smallest, dir| {
+        input.into_values().fold(root_size, |smallest, dir| {
             if remaining_size + dir >= UPDATE_SIZE && dir < smallest {
                 dir
             } else {
@@ -46,90 +30,49 @@ impl Day for Day07 {
     }
 
     fn parse(raw: &str) -> Self::Input {
-        struct State {
-            pub root: Rc<RefCell<Directory>>,
-            pub path: Vec<String>,
-        }
+        let mut path = Vec::new();
+        let mut dirs = HashMap::new();
 
-        let root = raw
-            .lines()
-            .fold(
-                State {
-                    root: Rc::new(RefCell::new(Directory::new())),
-                    path: Vec::new(),
+        let mut lines = raw.lines().peekable();
+        while let Some(line) = lines.next() {
+            match &line[2..4] {
+                "cd" => match &line[5..] {
+                    ".." => {
+                        path.pop();
+                    }
+                    "/" => path.clear(),
+                    p => {
+                        path.push(p.to_string());
+                    }
                 },
-                |State { root, mut path }, line| {
-                    let current_dir = path.iter().fold(Rc::clone(&root), |dir, next_dir| {
-                        if let Entry::Dir(dir) = dir.borrow().get(next_dir).unwrap() {
-                            Rc::clone(dir)
+                "ls" => {
+                    // Add file sizes to current directory
+                    let mut dir_size = 0;
+                    while {
+                        if let Some(l) = lines.peek() {
+                            !l.starts_with('$')
                         } else {
-                            unreachable!()
+                            false
                         }
-                    });
+                    } {
+                        let size_or_type = lines.next().unwrap().split_whitespace().next().unwrap();
 
-                    if line.starts_with('$') {
-                        // Command
-                        if line.contains("cd") {
-                            // cd command
-                            match &line[5..] {
-                                ".." => {
-                                    path.pop();
-                                }
-                                "/" => path.clear(),
-                                p => {
-                                    path.push(p.to_string());
-                                }
-                            }
-                        }
-                    } else {
-                        // Directory listing
-                        let (size_or_type, name) = line.split_once(' ').unwrap();
-                        let name = name.to_string();
-
-                        if size_or_type == "dir" {
-                            // Create dir
-                            current_dir
-                                .borrow_mut()
-                                .insert(name, Entry::Dir(Rc::new(RefCell::new(Directory::new()))));
-                        } else {
-                            // Create file
-                            let size = size_or_type.parse().unwrap();
-
-                            current_dir.borrow_mut().insert(name, Entry::File(size));
+                        if size_or_type != "dir" {
+                            dir_size += size_or_type.parse::<usize>().unwrap();
                         }
                     }
 
-                    State { root, path }
-                },
-            )
-            .root;
-
-        fn size_dir(dir: Rc<RefCell<Directory>>) -> FlattenedDirectories {
-            let mut size = 0;
-            let mut dirs = Vec::new();
-
-            for entry in dir.borrow().values() {
-                match entry {
-                    Entry::Dir(dir) => {
-                        let sized_dir = size_dir(Rc::clone(dir));
-                        size += sized_dir.size;
-
-                        // Add this dir's sub dirs
-                        dirs.extend(sized_dir.dirs);
-
-                        // Add this dir
-                        dirs.push(sized_dir.size);
-                    }
-                    Entry::File(file_size) => {
-                        size += file_size;
+                    // Update entry and all parent entries
+                    for i in 0..=path.len() {
+                        let path = format!("/{}", path[0..i].join("/"));
+                        *dirs.entry(path).or_insert(0) += dir_size;
                     }
                 }
+                _ => unreachable!(),
             }
-
-            FlattenedDirectories { dirs, size }
         }
 
-        size_dir(root)
+        dirs
     }
 }
 
