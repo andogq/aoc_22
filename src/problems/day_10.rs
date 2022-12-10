a0623f6c27fcbaf064bb799d9903e4020cb622dd
Tom Anderson <tom@ando.gq>
2022-12-10 19:26:19 +1100

    Refactory day 10

--- src/problems/day_10.rs
+++ src/problems/day_10.rs
@@ -6,86 +6,149 @@ pub enum Instruction {
     Nop,
 }
 
+mod cpu {
+    use super::Instruction;
+
+    const CYCLE_OFFSET: usize = 20;
+    const DISPLAY_WIDTH: usize = 40;
+
+    pub struct CpuState {
+        cycles: usize,
+        reg_x: isize,
+    }
+
+    impl CpuState {
+        pub fn new() -> Self {
+            CpuState {
+                cycles: 0,
+                reg_x: 1,
+            }
+        }
+    }
+
+    pub trait Peripheral {
+        fn tick(&mut self, state: &CpuState);
+    }
+
+    pub struct Cpu<'a> {
+        state: CpuState,
+        pub signal_values: Vec<isize>,
+        peripherals: Vec<&'a mut dyn Peripheral>,
+    }
+
+    impl<'a> Cpu<'a> {
+        pub fn new() -> Self {
+            Self {
+                state: CpuState::new(),
+                signal_values: Vec::new(),
+                peripherals: Vec::new(),
+            }
+        }
+
+        pub fn with_peripheral(mut self, peripheral: &'a mut dyn Peripheral) -> Self {
+            self.peripherals.push(peripheral);
+
+            self
+        }
+
+        pub fn run(&mut self, instructions: &[Instruction]) {
+            for instruction in instructions {
+                self.tick();
+
+                match instruction {
+                    Instruction::Add(amount) => {
+                        self.tick();
+
+                        self.state.reg_x += amount;
+                    }
+                    Instruction::Nop => {}
+                }
+            }
+        }
+
+        pub fn tick(&mut self) {
+            self.state.cycles += 1;
+
+            if self.state.cycles >= CYCLE_OFFSET
+                && (self.state.cycles - CYCLE_OFFSET) % DISPLAY_WIDTH == 0
+            {
+                self.signal_values
+                    .push(self.state.reg_x * self.state.cycles as isize);
+            }
+
+            for p in self.peripherals.iter_mut() {
+                p.tick(&self.state);
+            }
+        }
+    }
+
+    pub mod crt {
+        use super::*;
+        use std::fmt::Display;
+
+        pub struct CrtScreen {
+            screen: Vec<char>,
+        }
+
+        impl CrtScreen {
+            pub fn new() -> Self {
+                CrtScreen { screen: Vec::new() }
+            }
+
+            pub fn print(&mut self, c: char) {
+                self.screen.push(c)
+            }
+        }
+
+        impl Display for CrtScreen {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(
+                    f,
+                    "{}",
+                    self.screen
+                        .chunks(40)
+                        .flat_map(|line| [line, &['\n']].concat())
+                        .collect::<String>()
+                )
+            }
+        }
+
+        impl Peripheral for CrtScreen {
+            fn tick(&mut self, state: &CpuState) {
+                self.print({
+                    let crt_position = self.screen.len() as isize % 40;
+                    if crt_position <= (state.reg_x % 40) + 1
+                        && crt_position >= (state.reg_x % 40).saturating_sub(1)
+                    {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                });
+            }
+        }
+    }
+}
+
 pub struct Day10;
 impl Day for Day10 {
     type Input = Vec<Instruction>;
     type Output = isize;
 
     fn part_1(input: Self::Input) -> Self::Output {
-        let mut reg_x = 1;
-        let mut signal_values = Vec::new();
+        let mut cpu = cpu::Cpu::new();
+        cpu.run(&input);
 
-        let mut cycle_count = 0;
-        let mut tick = |reg_x| {
-            cycle_count += 1;
-
-            if (cycle_count - 20) % 40 == 0 {
-                let signal = reg_x * cycle_count;
-                signal_values.push(signal);
-            }
-        };
-
-        for instruction in input {
-            match instruction {
-                Instruction::Add(amount) => {
-                    tick(reg_x);
-                    tick(reg_x);
-                    reg_x += amount;
-                }
-                Instruction::Nop => {
-                    tick(reg_x);
-                }
-            }
-        }
-
-        signal_values.into_iter().sum()
+        cpu.signal_values.into_iter().sum()
     }
 
     fn part_2(input: Self::Input) -> Self::Output {
-        let mut reg_x: isize = 1;
-        let mut signal_values = Vec::new();
+        let mut screen = cpu::crt::CrtScreen::new();
+        let mut cpu = cpu::Cpu::new().with_peripheral(&mut screen);
+        cpu.run(&input);
+        println!("{}", screen);
 
-        let mut cycle_count = 1;
-        let mut line = Vec::with_capacity(40);
-
-        let mut tick = |reg_x: isize| {
-            cycle_count += 1;
-
-            if cycle_count > 20 && (cycle_count - 20) % 40 == 0 {
-                let signal = reg_x * (cycle_count as isize);
-                signal_values.push(signal);
-            }
-
-            line.push({
-                let crt_position = line.len() as isize;
-                if crt_position <= (reg_x % 40) + 1
-                    && crt_position >= (reg_x % 40).saturating_sub(1)
-                {
-                    '#'
-                } else {
-                    '.'
-                }
-            });
-
-            if line.len() == 40 {
-                println!("{}", line.iter().cloned().collect::<String>());
-                line = Vec::with_capacity(40);
-            }
-        };
-
-        for instruction in input {
-            match instruction {
-                Instruction::Add(amount) => {
-                    tick(reg_x);
-                    tick(reg_x);
-                    reg_x += amount;
-                }
-                Instruction::Nop => {
-                    tick(reg_x);
-                }
-            }
-        }
-
+        // TODO: Parse generated output, and test against known string
         0
     }
 
@@ -99,6 +162,12 @@ impl Day for Day10 {
                 }
             })
             .collect()
+    }
+
+    fn run(input: &str) -> (Self::Output, Self::Output) {
+        let input = Self::parse(input);
+
+        (Self::part_1(input.clone()), Self::part_2(input))
     }
 }
 
